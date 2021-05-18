134195eba576136d75155650662caf4e58523b96
Vlad-Enia <vladgio99@gmail.com>
2021-05-18 12:34:08 +0300

    First step in git and spring setup.
    Need to configure githum actions and heroku next.

---
+++ src/main/java/ro/uaic/info/contentmanager/ContentManagerApplication.java
@@ -0,0 +1,13 @@
+package ro.uaic.info.contentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentManagerApplication.class, args);
+	}
+
+}
